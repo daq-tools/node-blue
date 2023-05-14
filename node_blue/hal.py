f7f1dd67df1434a65daaf8f1eed43801c3b14c30
Andreas Motl <andreas.motl@panodata.org>
2023-05-14 05:03:12 +0200

    Attempt to fix crashes on Windows

--- node_blue/hal.py
+++ node_blue/hal.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2023, The Panodata developers and contributors.
 # Distributed under the terms of the Apache-2.0 license, see LICENSE.
 import dataclasses
+import threading
 import typing as t
 import asyncio
 from pathlib import Path
@@ -20,6 +21,7 @@ def jsrun(program: t.Union[str, Path]) -> Context:
     bootloader = open(program).read()
     module: t.Dict = {}
     retval = javascript.eval_js(bootloader)
+    threading.Event().wait(0.01)
     # print("retval:", retval)
 
     current_task = asyncio.current_task()
