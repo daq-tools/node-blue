d6a5eab525960a693eab224a9600fe3e9451a153
Andreas Motl <andreas.motl@panodata.org>
2023-05-14 04:51:57 +0200

    Add program `node-blue setup`, to install JavaScript prerequisites

---
+++ node_blue/cli.py
@@ -0,0 +1,74 @@
+# Copyright (c) 2023, The Panodata developers and contributors.
+# Distributed under the terms of the Apache-2.0 license, see LICENSE.
+import logging
+import typing as t
+
+import click
+
+from node_blue.core import launch_blue, NodeBlue
+from node_blue.util import boot_click, docstring_format_verbatim, make_sync
+
+logger = logging.getLogger(__name__)
+
+
+def help_launch():
+    """
+    Run Node-RED instance, with flow file.
+
+    Synopsis
+    ========
+
+    node-blue launch \\
+        --flow=https://github.com/daq-tools/node-blue/blob/main/examples/flows/http-html-templating.json
+
+    """  # noqa: E501
+
+
+def help_setup():
+    """
+    Set up Node-BLUE.
+
+    node-blue setup
+    """  # noqa: E501
+
+
+@click.group()
+@click.version_option(package_name="node-blue")
+@click.option("--verbose", is_flag=True, required=False, help="Turn on logging")
+@click.option("--debug", is_flag=True, required=False, help="Turn on logging with debug level")
+@click.pass_context
+def cli(ctx: click.Context, verbose: bool, debug: bool):
+    verbose = True
+    return boot_click(ctx, verbose, debug)
+
+
+@cli.command("info", help="Report about platform information")
+def info():
+    # AboutReport.platform()
+    pass
+
+
+@cli.command(
+    "setup",
+    help=docstring_format_verbatim(help_setup.__doc__),
+    context_settings={"max_content_width": 120},
+)
+@click.argument("command", nargs=-1)
+@click.pass_context
+def setup(ctx: click.Context, command: t.Tuple[str]):
+    logger.info("Setup")
+    NodeBlue().setup()
+
+
+@cli.command(
+    "launch",
+    help=docstring_format_verbatim(help_launch.__doc__),
+    context_settings={"max_content_width": 120},
+)
+@click.option("--flow", type=str, required=False)
+@click.pass_context
+@make_sync
+async def launch(ctx: click.Context, flow: str):
+    logger.info("Starting")
+    # await run_single(source, sink)
+    await launch_blue()
--- node_blue/util.py
+++ node_blue/util.py
@@ -1,10 +1,14 @@
 # Copyright (c) 2023, The Panodata developers and contributors.
 # Distributed under the terms of the Apache-2.0 license, see LICENSE.
 import asyncio
+import functools
 import logging
 import sys
+import textwrap
 import threading
 import typing as t
+
+import click
 
 
 def setup_logging(level=logging.INFO):
@@ -19,3 +23,46 @@ async def run_later(fun: t.Callable, seconds: float):
 
 def wait(seconds: float):
     threading.Event().wait(seconds)
+
+
+def boot_click(ctx: click.Context, verbose: bool = False, debug: bool = False):
+    """
+    Bootstrap the CLI application.
+    """
+
+    # Adjust log level according to `verbose` / `debug` flags.
+    log_level = logging.WARNING
+    if verbose:
+        log_level = logging.INFO
+    if debug:
+        log_level = logging.DEBUG
+
+    # Setup logging, according to `verbose` / `debug` flags.
+    setup_logging(level=log_level)
+
+
+def docstring_format_verbatim(text: t.Optional[str]) -> str:
+    """
+    Format docstring to be displayed verbatim as a help text by Click.
+
+    - https://click.palletsprojects.com/en/8.1.x/documentation/#preventing-rewrapping
+    - https://github.com/pallets/click/issues/56
+    """
+    text = text or ""
+    text = textwrap.dedent(text)
+    lines = [line if line.strip() else "\b" for line in text.splitlines()]
+    return "\n".join(lines)
+
+
+def make_sync(func):
+    """
+    Click entrypoint decorator for wrapping asynchronous functions.
+
+    https://github.com/pallets/click/issues/2033
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        return asyncio.run(func(*args, **kwargs))
+
+    return wrapper
